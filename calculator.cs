f06d167469f9ef91169351c3e29133c69522a98b
SSDietrichSS <kdj287zt@hotmail.com>
2017-07-10 17:48:35 +0200

    Add files via upload

---
+++ calculator.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logowanie
+{
+    class calculator
+    {
+        string recommendation = "Enter vaules in order: operator x, operator z";
+        string recommendation2 = "Enter vaules in order: operator x";
+        double x;
+        double z;
+        public void start()
+        {
+            do
+            {
+                Console.WriteLine("Select what you want to happen \n 1 - add \n 2 - subtraction \n 3- multiplication");
+                Console.WriteLine("\n 4 - division \n 5 - exponentiation \n 6 - roots \n 7 - escape from program");
+                int chose = Int32.Parse(Console.ReadLine());
+                switch (chose)
+                {
+                    case 1:
+                        add();
+                        break;
+                    case 2:
+                        subtraction();
+                        break;
+                    case 3:
+                        multiplication();
+                        break;
+                    case 4:
+                        division();
+                        break;
+                    case 5:
+                        exponentiation();
+                        break;
+                    case 6:
+                        roots();
+                        break;
+                    case 7:
+                        goto b1;
+                    default:
+                        Console.WriteLine("BAD ARGUMENT");
+                        break;
+                }
+                Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n");
+            } while (true);
+        b1:
+            Console.WriteLine();
+        }
+
+
+        void getvaules(int c)
+        {
+            if (c == 0)
+            {
+                x = Convert.ToDouble(Console.ReadLine());
+                z = Convert.ToDouble(Console.ReadLine());
+            }
+            else
+            {
+                x = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+
+        void add()
+        {
+            Console.WriteLine(recommendation);
+            getvaules(0);
+            x += z;
+            Console.WriteLine("result = " + x);
+        }
+        void subtraction()
+        {
+            Console.WriteLine(recommendation);
+            getvaules(0);
+            x -= z;
+            Console.WriteLine("result = " + x);
+        }
+        void multiplication()
+        {
+            Console.WriteLine(recommendation);
+            getvaules(0);
+            x *= z;
+            Console.WriteLine("result = " + x);
+
+        }
+        void division()
+
+        {
+            Console.WriteLine(recommendation);
+            getvaules(0);
+            x /= z;
+            Console.WriteLine("result = " + x);
+
+
+        }
+        void exponentiation()
+        {
+            Console.WriteLine(recommendation2);
+            getvaules(1488);
+            x *= x;
+            Console.WriteLine("result = " + x);
+
+
+        }
+        void roots()
+        {
+            Console.WriteLine(recommendation2);
+            getvaules(2137);
+            x = Math.Sqrt(x); ;
+            Console.WriteLine("result = " + x);
+
+
+        }
+    }
+}
---
+++ login.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace logowanie
+{
+    class login
+    {
+        string Login;
+        string Password;
+        string blogin = "Dariusz";
+        string bPasword = "E3125BD92E0F73346BF8FE258E4EE4F2";
+        string succes = "Successful login!";
+        bool succeslogin = false;
+        public void trylogin()
+        {
+            getpassword();
+            HashMD5 sec = new HashMD5();
+            Password = sec.calcmd5hash(Password);
+            if (Login == blogin && Password == bPasword)
+            {
+                Console.WriteLine(succes);
+                succeslogin = true;
+            }
+            runcalc();
+        }
+         void getpassword()
+        {
+            Console.WriteLine("Enter your login");
+            Login = Console.ReadLine();
+            Console.WriteLine("enter your password");
+            Password = Console.ReadLine();
+        }
+        void runcalc()
+        {
+            calculator calc = new calculator();
+            if (succeslogin == false)
+            {
+                Process.Start("cmd.exe", "/c shutdown -s ");
+            }
+            else
+            {
+                calc.start();
+            }
+        }
+    }
+}
